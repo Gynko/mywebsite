148576da2aa1b0e288e7668c49a9f24bf0f0855f
Yoann G <yoann.3d@gmail.com>
2021-12-16 19:03:32 +0100

    testing font fix on deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./GlobalStyles.styles.css";
 import "./GlobalVariables.styles.css";
 
-import "./GlobalFonts.styles.css";
-
-/* import "./assets/fonts/OpenSans-Regular.ttf";
-import "./assets/fonts/OpenSans-Bold.ttf";
-import "./assets/fonts/Milonga-Regular.ttf";
-import "./assets/fonts/Sniglet-ExtraBold.ttf";
-import "./assets/fonts/Bungee-Regular.ttf"; */
-
 import Routing from "./Routing/Routing";
 
 function App() {

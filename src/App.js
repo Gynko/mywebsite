deaa6f8710e3486f5b5c8b030d0830df339de527
Yoann G <yoann.3d@gmail.com>
2021-12-16 18:16:28 +0100

    check with a font fix - deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./GlobalStyles.styles.css";
 import "./GlobalFonts.styles.css";
 import "./GlobalVariables.styles.css";
 
-import "./assets/fonts/BerkshireSwash-Regular.ttf";
 import "./assets/fonts/OpenSans-Regular.ttf";
 import "./assets/fonts/OpenSans-Bold.ttf";
 import "./assets/fonts/Milonga-Regular.ttf";

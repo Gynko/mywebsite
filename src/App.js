56f1ea19b854f8f997afaafc290f9fd62e2e542f
Yoann G <yoann.3d@gmail.com>
2021-12-17 19:38:32 +0100

    troubleshooting some heroku issue with node - adding node engine to package.json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./styling/GlobalStyles.styles.css";
 import "./styling/GlobalVariables.styles.css";
 
 import Routing from "./Routing/Routing";
+import { useEffect } from "react";
 
 function App() {
-  window.addEventListener("resize", () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      var vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
   });
 
   return <Routing />;

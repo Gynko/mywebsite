614846be30affc671c9f72fc719493319d87250b
Yoann G <yoann.3d@gmail.com>
2021-12-17 16:18:10 +0100

    trying to fix the vh issue on mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import "./GlobalStyles.styles.css";
-import "./GlobalVariables.styles.css";
+import "./styling/GlobalStyles.styles.css";
+import "./styling/GlobalVariables.styles.css";
 
 import Routing from "./Routing/Routing";
 
 function App() {
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+
   return <Routing />;
 }
 

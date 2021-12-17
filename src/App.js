ec6dad1ad4671fe0c76ba8691e1d9ddad724b111
Yoann G <yoann.3d@gmail.com>
2021-12-17 21:27:01 +0100

    deployment troubleshooting

--- src/App.js
+++ src/App.js
@@ -2,22 +2,8 @@ import "./styling/GlobalStyles.styles.css";
 import "./styling/GlobalVariables.styles.css";
 
 import Routing from "./Routing/Routing";
-import { useEffect } from "react";
 
 function App() {
-  const onResize = () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  };
-
-  useEffect(() => {
-    onResize();
-    window && window.addEventListener("resize", onResize);
-    return () => {
-      window && window.removeEventListener("resize", onResize);
-    };
-  }, []);
-
   return <Routing />;
 }
 

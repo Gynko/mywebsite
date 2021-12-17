27e936e3d564e73c6f7f0951464183f1a7ed1fe9
Yoann G <yoann.3d@gmail.com>
2021-12-17 22:12:47 +0100

    fixed many flexbox cheatsheet issues on mobile on vertical axis - testing pagehome

--- src/pages/home/PageHome.jsx
+++ src/pages/home/PageHome.jsx
@@ -2,11 +2,11 @@ import "./PageHome.styles.css";
 import IconRabbitHole from "../../components/Icons/IconRabbitHole";
 import Footer from "../../components/Footer/Footer";
 import ButtonLink from "../../components/ButtonLink/ButtonLink";
-import PageContainer from "../../components/PageContainer/PageContainer";
+import PageContainerNoScroll from "../../components/PageContainerNoScroll/PageContainerNoScroll";
 
 function PageHome() {
   return (
-    <PageContainer>
+    <PageContainerNoScroll>
       <div className="home-allbutfooter-container">
         <div className="home-logo-title-container">
           <div className="icon-rabbit-hole">
@@ -39,7 +39,7 @@ function PageHome() {
         </div>
       </div>
       <Footer />
-    </PageContainer>
+    </PageContainerNoScroll>
   );
 }
 

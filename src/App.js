0af0fbba4c88a8346e00f3b146f504b612e644c8
Yoann G <yoann.3d@gmail.com>
2021-12-19 05:19:50 +0100

    trying react use vh fix

--- src/App.js
+++ src/App.js
@@ -2,22 +2,10 @@ import "./styling/GlobalStyles.styles.css";
 import "./styling/GlobalVariables.styles.css";
 
 import Routing from "./Routing/Routing";
-import { useEffect } from "react";
+import useVH from "react-viewport-height/useVH";
 
 function App() {
-  const onResize = () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  };
-
-  useEffect(() => {
-    onResize();
-    window && window.addEventListener("resize", onResize);
-    return () => {
-      window && window.removeEventListener("resize", onResize);
-    };
-  });
-
+  useVH();
   return <Routing />;
 }
 

442de3ee280fa90f4ff327264315d1336304edab
Yoann G <yoann.3d@gmail.com>
2021-12-19 06:22:58 +0100

    testing a fix for the behaviour of the layout with the  svg when zooming in on mobile

--- src/components/Icons/IconMail.jsx
+++ src/components/Icons/IconMail.jsx
@@ -4,7 +4,7 @@ function IconMail() {
       id="Layer_1"
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
-      width="35"
+      width="60"
       height="100%"
       viewBox="0 0 20 16"
     >

c2963a58224767bb0246c40171d99391f6a116fe
Yoann G <yoann.3d@gmail.com>
2021-12-19 05:21:52 +0100

    trying react use vh fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./styling/GlobalStyles.styles.css";
 import "./styling/GlobalVariables.styles.css";
 
 import Routing from "./Routing/Routing";
-import useVH from "react-viewport-height/useVH";
+import useVH from "react-viewport-height";
 
 function App() {
   useVH();

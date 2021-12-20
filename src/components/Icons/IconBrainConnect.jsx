534ec8b9226ebd2a20a502a0f62113f1e0c2fd5d
Yoann G <yoann.3d@gmail.com>
2021-12-20 09:47:38 +0100

    adding mobile media queries to center the svg on larger phones

--- src/components/Icons/IconBrainConnect.jsx
+++ src/components/Icons/IconBrainConnect.jsx
@@ -4,7 +4,7 @@ function IconBrainConnect() {
       id="Layer_1"
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
-      width="60"
+      width="50"
       height="100%"
       viewBox="0 0 68 68"
     >

ea3e223e78cdcbc160b86f72dc1826f2c4a57bdd
Yoann G <yoann.3d@gmail.com>
2021-12-22 09:03:11 +0100

    page-home responsivity

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function ButtonLink({ label, to }) {
   return (
-    <Link to={to}>
-      <button className="button-link">{label}</button>
+    <Link className="button-link" to={to}>
+      {label}
     </Link>
   );
 }
